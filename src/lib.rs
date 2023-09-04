2a93cbe70debb70aa44503d8431be4f8f12c867e
Joe <burtonjae@hotmail.co.uk>
2023-09-05 00:43:00 +0100

    Make types module public

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 #![allow(clippy::collapsible_else_if)]
 
 mod helpers;
-mod types;
+pub mod types;
 pub mod version_names;
 mod versions;
-
-pub use types::*;
 
 pub mod json {
     pub use crate::helpers::json_parser::*;
